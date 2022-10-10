1fe1b13af91a823de3d1cfe61d20b57d97f150cf
fikriibrahimahid <fikriibrahim1999@gmail.com>
2022-10-10 14:22:00 +0700

    fix up function on combine_items_in_cart migration

--- db/migrate/20221010071350_combine_items_in_cart.rb
+++ db/migrate/20221010071350_combine_items_in_cart.rb
@@ -1,6 +1,6 @@
 class CombineItemsInCart < ActiveRecord::Migration[7.0]
   def up
-    Cart.all each do |cart|
+    Cart.all.each do |cart|
       sums = cart.line_items.group(:product_id).sum(:quantity)
       sums.each do |product_id, quantity|
         if quantity > 1
