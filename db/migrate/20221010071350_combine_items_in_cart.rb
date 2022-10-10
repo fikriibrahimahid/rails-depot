a7c2d6ace862815e827b5791ae5163bed9ce1efd
fikriibrahimahid <fikriibrahim1999@gmail.com>
2022-10-10 14:27:14 +0700

    create down function for combine_items_in_cart migration

--- db/migrate/20221010071350_combine_items_in_cart.rb
+++ db/migrate/20221010071350_combine_items_in_cart.rb
@@ -13,4 +13,16 @@ class CombineItemsInCart < ActiveRecord::Migration[7.0]
       end
     end
   end
+
+  def down
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
+        )
+    end
+    line_item.destroy
+  end
 end
